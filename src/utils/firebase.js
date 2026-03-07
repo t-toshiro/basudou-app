0c05a3ce750d13406f0ed27c415a0ca80ef2d43a
toshiro <tsrbasuke@icloud.com>
2026-03-08 01:03:53 +0900

    firebaseへの接続設定

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// ⚠️ ご自身のFirebaseプロジェクトの config に書き換えてください！
 const firebaseConfig = {
-  apiKey: "AIzaSyB...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyBgmHASydFAjPoO01MQK67HMp3H8fGoYQU",
+  authDomain: "basketball-app-8212e.firebaseapp.com",
+  projectId: "basketball-app-8212e",
+  storageBucket: "basketball-app-8212e.firebasestorage.app",
+  messagingSenderId: "479830707810",
+  appId: "1:479830707810:web:4e1539dd92c66e957d4b3b",
+  measurementId: "G-GXCT67ZP3K",
 };
 
 // Firebaseの初期化とデータベース（Firestore）の呼び出し
